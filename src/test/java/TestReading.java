639a505ed2a9c804f44967669f92aa3c49dc34c8
Jim Pivarski <jpivarski@gmail.com>
2016-10-26 16:56:48 -0500

    include simple.root test and drop notsosimple.root (no evidence that it was readable by anything)

--- src/test/java/TestReading.java
+++ src/test/java/TestReading.java
@@ -10,7 +10,7 @@ import hep.io.root.interfaces.*;
 
 public class TestReading {
     @Test
-    public void testHello() throws java.io.IOException, RootClassNotFound {
+    public void testVerySimple() throws java.io.IOException, RootClassNotFound {
         RootFileReader reader = new RootFileReader("src/test/resources/verysimple.root");
         TTree tree = (TTree)reader.get("ntuple");
         List leaves = (List)tree.getLeaves();
@@ -23,25 +23,56 @@ public class TestReading {
         assertEquals(leaf1.getName(), "y");
         assertEquals(leaf2.getName(), "z");
 
-        assertEquals(leaf0.getValue(0), 1.0, 1e-12);
-        assertEquals(leaf1.getValue(0), 2.0, 1e-12);
-        assertEquals(leaf2.getValue(0), 3.0, 1e-12);
+        assertEquals(leaf0.getValue(0), 1.0, 1e-6);
+        assertEquals(leaf1.getValue(0), 2.0, 1e-6);
+        assertEquals(leaf2.getValue(0), 3.0, 1e-6);
 
-        assertEquals(leaf0.getValue(1), 4.0, 1e-12);
-        assertEquals(leaf1.getValue(1), 5.0, 1e-12);
-        assertEquals(leaf2.getValue(1), 6.0, 1e-12);
+        assertEquals(leaf0.getValue(1), 4.0, 1e-6);
+        assertEquals(leaf1.getValue(1), 5.0, 1e-6);
+        assertEquals(leaf2.getValue(1), 6.0, 1e-6);
 
-        assertEquals(leaf0.getValue(2), 7.0, 1e-12);
-        assertEquals(leaf1.getValue(2), 8.0, 1e-12);
-        assertEquals(leaf2.getValue(2), 9.0, 1e-12);
+        assertEquals(leaf0.getValue(2), 7.0, 1e-6);
+        assertEquals(leaf1.getValue(2), 8.0, 1e-6);
+        assertEquals(leaf2.getValue(2), 9.0, 1e-6);
 
-        assertEquals(leaf0.getValue(3), 10.0, 1e-12);
-        assertEquals(leaf1.getValue(3), 11.0, 1e-12);
-        assertEquals(leaf2.getValue(3), 12.0, 1e-12);
+        assertEquals(leaf0.getValue(3), 10.0, 1e-6);
+        assertEquals(leaf1.getValue(3), 11.0, 1e-6);
+        assertEquals(leaf2.getValue(3), 12.0, 1e-6);
 
-        assertEquals(leaf0.getValue(4), 1.0, 1e-12);
-        assertEquals(leaf1.getValue(4), 2.0, 1e-12);
-        assertEquals(leaf2.getValue(4), 3.0, 1e-12);
-
+        assertEquals(leaf0.getValue(4), 1.0, 1e-6);
+        assertEquals(leaf1.getValue(4), 2.0, 1e-6);
+        assertEquals(leaf2.getValue(4), 3.0, 1e-6);
     }
+
+    @Test
+    public void testSimple() throws java.io.IOException, RootClassNotFound {
+        RootFileReader reader = new RootFileReader("src/test/resources/simple.root");
+        TTree tree = (TTree)reader.get("tree");
+        List leaves = (List)tree.getLeaves();
+
+        TLeafI leaf0 = (TLeafI)leaves.get(0);
+        TLeafF leaf1 = (TLeafF)leaves.get(1);
+        TLeafC leaf2 = (TLeafC)leaves.get(2);
+
+        assertEquals(leaf0.getName(), "one");
+        assertEquals(leaf1.getName(), "two");
+        assertEquals(leaf2.getName(), "three");
+
+        assertEquals(leaf0.getValue(0), 1);
+        assertEquals(leaf1.getValue(0), 1.1, 1e-6);
+        assertEquals(leaf2.getValue(0), "uno");
+
+        assertEquals(leaf0.getValue(1), 2);
+        assertEquals(leaf1.getValue(1), 2.2, 1e-6);
+        assertEquals(leaf2.getValue(1), "dos");
+
+        assertEquals(leaf0.getValue(2), 3);
+        assertEquals(leaf1.getValue(2), 3.3, 1e-6);
+        assertEquals(leaf2.getValue(2), "tres");
+
+        assertEquals(leaf0.getValue(3), 4);
+        assertEquals(leaf1.getValue(3), 4.4, 1e-6);
+        assertEquals(leaf2.getValue(3), "quatro");
+    }
+
 }
