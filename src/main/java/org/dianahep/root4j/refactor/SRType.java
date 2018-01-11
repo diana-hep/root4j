f4106579dd661f84c9caa17f6c476c97aaf92ce2
Pratyush Das <reikdas@gmail.com>
2018-01-11 10:19:49 +0530

    Reformated generics of SRType

--- src/main/java/org/dianahep/root4j/refactor/SRType.java
+++ src/main/java/org/dianahep/root4j/refactor/SRType.java
@@ -4,7 +4,7 @@ import org.dianahep.root4j.core.*;
 import java.io.*;
 import org.apache.log4j.*;
 
-public abstract class SRType<T>{
+public abstract class SRType{
 
     protected Logger logger = LogManager.getLogger("org.dianahep.root4j.refactor.TypeSystem");
 
@@ -16,17 +16,17 @@ public abstract class SRType<T>{
 
     abstract void debugMe(String str) throws IOException;
 
-    abstract T read(RootInput b) throws IOException;
+    abstract <T> T read(RootInput b) throws IOException;
 
-    abstract T read()throws IOException;
+    abstract <T> T read()throws IOException;
 
     boolean hasNext() {
         return false;
     };
 
-    abstract T readArray(int size)throws IOException;
+    abstract <T> T readArray(int size)throws IOException;
 
-    abstract T readArray(RootInput buffer, int size)throws IOException;
+    abstract <T> T readArray(RootInput buffer, int size)throws IOException;
 
     SRType(String name){
         this.name=name;
